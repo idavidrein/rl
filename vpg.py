eca0bce450de225cfdc74f36308b4385defb9d0a
“sfe2” <sfe2@duke.edu>
2019-04-08 23:58:42 -0400

    updating in git

--- vpg.py
+++ vpg.py
@@ -2,50 +2,72 @@ import gym
 import tensorflow as tf
 
 
-def explore(policy):
-	trajectories = {}
-
-	return trajectories
+def explore(policy, env, steps, num_episodes):
+    trajectories = []
+    rewards = []
+    for i in range(num_episodes):
+        observation = env.reset()
+        trajectory = []
+        reward_i = []
+        for i in range(steps):
+            action = get_action(policy, observation)
+            trajectory.append((observation, action))
+            observation, reward, done, info = env.step(action)
+            reward_i.append(reward)
+            if done:
+                print("Episode over after {0} timesteps".format(i+1))
+                break
+        trajectories.append(trajectory)
+        rewards.append(reward_i)
+        
+    return trajectories, rewards
 
 def compute_grad(trajectories, rewards, policy):
-	gradient = 0
+    gradient = 0
 
-	return gradient
-
-def compute_rewards(trajectories):
-	rewards = {}
-	for trajectory in trajectories:
-		#sum compute_state_reward
-
-	return rewards
+    return gradient
+    
+def get_action(policy, observation):
+    action = 0
+    # feedforward through network
+    
+    return action
 
 def compute_state_reward(state, action):
-	#calculate reward
-	return reward
+    #calculate reward
+    return reward
 
 def update_policy(policy, grad):
-	#update params via backpropagation
+    #tensorflow thing to take gradient step
 
-	return policy
+    return policy
 
-def init_policy(seed):
-	policy = 3
-
-	return policy
+def init_policy(seed, num_actions = 2):
+    policy = tf.keras.models.Sequential()
+    width = 32
+    policy.add(tf.keras.layers.Dense(width, activation = tf.nn.relu))  
+    policy.add(tf.keras.layers.Dense(num_actions, activation = tf.nn.softmax))
+    
+    return policy
 
 def cost_function(policy):
-	cost = 0
+    cost = 0
 
-	return cost
+    return cost
 
 
-def run(epochs = 5, seed = 1):
-	policy = init_policy(seed)
+def run(epochs = 5, seed = 1, steps = 20, num_episodes = 100, environment = 'CartPole-v0'):
+    env = gym.make(environment)
+    policy = init_policy(seed, num_actions)
+    for i in range(epochs):
+        trajectories, rewards = explore(policy, env, steps, num_episodes)
+        grad   = compute_grad(trajectories, rewards, policy)
+        policy = update_policy(policy, grad)
+    
+    print("Done!")
+    
+    env.close()
 
-	for i in range(epochs):
-		trajectories = explore(policy)
-		rewards = compute_rewards(trajectories)
-		grad = compute_grad(trajectories, rewards, policy)
-		policy = update_policy(policy, grad)
-		
+
+
 
