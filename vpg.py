c0773bb3d03948687734bbe540cbd15da3ede893
David Rein <idavidrein@gmail.com>
2019-04-20 13:09:44 -0400

    fixed indentation issues

--- vpg.py
+++ vpg.py
@@ -58,11 +58,11 @@ def compute_grad(params, rewards, trajectories, dims, sess):
         for t in range(len(Traj)):
             obs    = Traj[t][0]
             action = Traj[t][1]
-            (W1_grad, W2_grad, b1_grad, b2_grad) = grad_log_policy(params, action, obs, sess) * rtg(R, t)
+            W1_grad, W2_grad, b1_grad, b2_grad = grad_log_policy(params, action, obs, sess) * rtg(R, t)
             W1_grad_sum = tf.add(W1_grad, W1_grad_sum)
-			W2_grad_sum = tf.add(W2_grad, W2_grad_sum)
-			b1_grad_sum = tf.add(b1_grad, b1_grad_sum)
-			b2_grad_sum = tf.add(b2_grad, b2_grad_sum)
+            W2_grad_sum = tf.add(W2_grad, W2_grad_sum)
+            b1_grad_sum = tf.add(b1_grad, b1_grad_sum)
+            b2_grad_sum = tf.add(b2_grad, b2_grad_sum)
   
     return ( W1_grad_sum/N , W2_grad_sum/N , b1_grad_sum/N , b2_grad_sum/N ) 
 
@@ -76,12 +76,14 @@ def init_mlp(dims):
     return params
   
 def mlp(observation, params):
-  	W1, W2, b1, b2 = params
+    W1, W2, b1, b2 = params
     layer_1  = tf.nn.relu(tf.add(tf.matmul(observation, W1), b1))
     layer_out = tf.nn.softmax(tf.add(tf.matmul(layer_1, W2), b2)) #Minor Change: Sam added the b2 here
     return layer_out
 
-def run(epochs = 5, learning_rate = .01, steps = 20, num_episodes = 100, environment = 'CartPole-v0'):
+def run(epochs = 5, learning_rate = .01, 
+		steps = 20, num_episodes = 100, 
+		environment = 'CartPole-v0'):
     env          = gym.make(environment)
     action_dim   = env.action_space.n
     obs_dim      = env.observation_space.n
@@ -90,15 +92,15 @@ def run(epochs = 5, learning_rate = .01, steps = 20, num_episodes = 100, environ
 
     observation = tf.placeholder(tf.float32, [1, dims])
     params = init_mlp(dims)
-  	layer_out = mlp(observation, params)
+    layer_out = mlp(observation, params)
 
     with tf.Session() as sess:
-	    for i in range(epochs):
-	        trajectories, rewards = explore(layer_out, env, steps, num_episodes, sess)
-            
-	        W1_grad, W2_grad, b1_grad, b2_grad  = compute_grad(params, rewards, trajectories, dims, sess)
+        for i in range(epochs):
+            trajectories, rewards = explore(layer_out, env, steps, num_episodes, sess)
+
+            W1_grad, W2_grad, b1_grad, b2_grad  = compute_grad(params, rewards, trajectories, dims, sess)
             W1_new = W1.assign(tf.add(W1, learning_rate * W1_grad))
-	        W2_new = W2.assign(tf.add(W2, learning_rate * W2_grad))
+            W2_new = W2.assign(tf.add(W2, learning_rate * W2_grad))
             b1_new = b1.assign(tf.add(b1, learning_rate * b1_grad))
             b2_new = b2.assign(tf.add(b2, learning_rate * b2_grad))
     
