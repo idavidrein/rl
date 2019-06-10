eaf0ac3463cab531db3d2eb05892ebf93967eca3
David Rein <idavidrein@gmail.com>
2019-06-09 20:37:40 -0500

    formatting

--- utils.py
+++ utils.py
@@ -6,47 +6,54 @@ from gym.spaces import Box, Discrete
 import gym
 import json
 
-def discrete_network(dims = None, output_activation = 'softmax'):
-    x = Input(shape = dims[0], name = "input")
-    hidden = Dense(4, activation = 'relu', name = 'hidden_1')(x)
-    output = Dense(dims[1], activation = output_activation, name = 'output')(hidden)
-    model = Model(inputs = x, outputs = output)
+
+def discrete_network(dims=None, output_activation='softmax'):
+    x = Input(shape=dims[0], name="input")
+    hidden = Dense(4, activation='relu', name='hidden_1')(x)
+    output = Dense(dims[1], activation=output_activation, name='output')(hidden)
+    model = Model(inputs=x, outputs=output)
     return model
 
-def continuous_network(dims = None):
-    x = Input(shape = dims[0], name = "input")
-    hidden = Dense(4, activation = 'relu', name = 'hidden')(x)
-    means = Dense(dims[1], name = 'means')(hidden)
+
+def continuous_network(dims=None):
+    x = Input(shape=dims[0], name="input")
+    hidden = Dense(4, activation='relu', name='hidden')(x)
+    means = Dense(dims[1], name='means')(hidden)
     action, likelihood = sampling()(means)
-    model = Model(inputs = x, outputs = [action, likelihood])
+    model = Model(inputs=x, outputs=[action, likelihood])
     return model
+
 
 class sampling(tf.keras.layers.Layer):
     def __init__(self):
         super(sampling, self).__init__()
 
     def build(self, input_shape):
-        self.kernel = self.add_variable("kernel", 
-            shape=[input_shape[-1]],
-            initializer=tf.initializers.ones(), dtype = tf.float32)
+        self.kernel = self.add_variable("kernel",
+                                        shape=[input_shape[-1]],
+                                        initializer=tf.initializers.ones(), dtype=tf.float32)
 
-    def call(self, input):
+    def call(self, x):
         exp = tf.exp(self.kernel)
-        action = input + tf.random.normal([int(input.shape[-1])]) * exp
-        likelihood = log_likelihood(action, input, self.kernel)
+        action = x + tf.random.normal([int(x.shape[-1])]) * exp
+        likelihood = log_likelihood(action, x, self.kernel)
         return action, likelihood
 
+
 def log_likelihood(x, mu, log_std):
-    likelihood = -0.5*(((x-mu)/(tf.exp(log_std)+1e-8))**2 + 2*log_std + np.log(2*np.pi))
+    likelihood = -0.5 * (((x - mu) / (tf.exp(log_std) + 1e-8)) ** 2 + 2 * log_std + np.log(2 * np.pi))
     return likelihood
 
-def kl(P, Q):
-    return -np.sum(P * np.log(Q / P))
+
+def kl(p, q):
+    return -np.sum(p * np.log(q / p))
+
 
 def save_models(models, filepaths):
-    assert(len(models) == len(filepaths))
+    assert (len(models) == len(filepaths))
     for ix in range(len(models)):
         models[ix].save(filepaths[ix])
+
 
 class Logger():
     def __init__(self, file_name, info):
@@ -57,8 +64,9 @@ class Logger():
         with open(self.fpath + '_info.json', 'w') as info_file:
             json.dump(self.info, info_file, indent=4)
 
-    def log_epoch(self, **kwargs):          
+    def log_epoch(self, **kwargs):
         return
+
 
 def summary_stats(info):
     summaries = dict()
@@ -69,19 +77,21 @@ def summary_stats(info):
     summaries['variance'] = np.var(info)
     return summaries
 
+
 def create_policy(action_space, obs_dim, action_dim):
     if isinstance(action_space, Discrete):
-        policy = discrete_network(dims = (obs_dim, action_dim))
+        policy = discrete_network(dims=(obs_dim, action_dim))
     else:
-        policy = continuous_network(dims = (obs_dim, action_dim))
+        policy = continuous_network(dims=(obs_dim, action_dim))
     return policy
 
-def get_dims(act_space, obs_space):
 
+def get_dims(act_space, obs_space):
     if isinstance(act_space, Discrete):
         action_dim = act_space.n
 
-    else: action_dim = act_space.shape[0]
+    else:
+        action_dim = act_space.shape[0]
 
     if isinstance(obs_space, Discrete):
         obs_dim = (1,)
@@ -89,11 +99,12 @@ def get_dims(act_space, obs_space):
     elif isinstance(obs_space, gym.spaces.Tuple):
         obs_dim = len(obs_space)
 
-    else: obs_dim = obs_space.shape
+    else:
+        obs_dim = obs_space.shape
 
     return obs_dim, action_dim
 
 
 if __name__ == '__main__':
-    layer = sampling(num_outputs = 1)
-    print(layer(np.array([[10]], dtype = np.float32)))
+    layer = sampling()
+    print(layer(np.array([[10]], dtype=np.float32)))
--- vpg.py
+++ vpg.py
@@ -10,12 +10,12 @@ from utils import *
 from gym.spaces import Discrete, Box
 import atexit
 
-def vpg(environment='CartPole-v0', hidden_units=32, gamma=0.9, 
-        seed_num=10, learning_rate=.01, num_episodes=400,
-        batch_size=10, max_steps=1000, num_layers = 1, 
-        fpath="logs/log", arg_dict=dict(), lam=.9, save=''):
 
-    logger = Logger(file_name = fpath, info = arg_dict)
+def vpg(environment='CartPole-v0', hidden_units=32, gamma=0.9,
+        seed_num=10, learning_rate=.01, num_episodes=400,
+        batch_size=10, max_steps=1000, num_layers=1,
+        fpath="logs/log", arg_dict=dict(), lam=.9, save=''):
+    logger = Logger(file_name=fpath, info=arg_dict)
     save_path = "models/" + save
     policy_path = save_path + "_policy"
 
@@ -24,7 +24,7 @@ def vpg(environment='CartPole-v0', hidden_units=32, gamma=0.9,
     random.seed(seed_num)
     os.environ['PYTHONHASHSEED'] = str(seed_num)
     tf.random.set_seed(seed_num)
-    
+
     env = gym.make(environment)
 
     obs_dim, action_dim = get_dims(env.action_space, env.observation_space)
@@ -32,8 +32,8 @@ def vpg(environment='CartPole-v0', hidden_units=32, gamma=0.9,
     # create policy network (actor)
     policy = create_policy(env.action_space, obs_dim, action_dim)
 
-    optimizer = tf.keras.optimizers.Adam(lr = learning_rate)
-    
+    optimizer = tf.keras.optimizers.Adam(lr=learning_rate)
+
     # create buffer so we can aggregate gradients
     grad_buffer = policy.trainable_variables
     for ix, grad in enumerate(grad_buffer):
@@ -45,10 +45,10 @@ def vpg(environment='CartPole-v0', hidden_units=32, gamma=0.9,
 
     # main execution loop
     for ep_number in range(num_episodes):
-        
+
         ep_reward = 0
         obs = np.array(env.reset()).reshape(1, -1)
-        
+
         ep_buffer = []
 
         # run one episode
@@ -63,8 +63,6 @@ def vpg(environment='CartPole-v0', hidden_units=32, gamma=0.9,
                     log = log_probs[0, action]
                 else:
                     action, log = policy(obs)
-
-            prev_obs = obs
 
             # take gradient w.r.t. params of log of action taken
             grads = tape.gradient(log, policy.trainable_variables)
@@ -91,23 +89,22 @@ def vpg(environment='CartPole-v0', hidden_units=32, gamma=0.9,
             # create discounting array
             weights = np.power(weights, range(length))
             # calculate rewards-to-to
-            rewards_to_go[t] = np.sum(np.multiply(weights, ep_buffer[t:,0]))
+            rewards_to_go[t] = np.sum(np.multiply(weights, ep_buffer[t:, 0]))
 
         # add episode information to current estimation of policy gradient
         for t, ep_info in enumerate(ep_buffer):
             for ix, grad in enumerate(ep_info[1]):
-
-                # subtract because optimizer minimizes loss function, 
+                # subtract because optimizer minimizes loss function,
                 # and we want to maximize expected reward
                 grad_buffer[ix] -= (1 / batch_size) * grad * rewards_to_go[t]
 
         # every batch_size number of episodes, 
         # run gradient descent on samples
         if ep_number % batch_size == 0 and ep_number != 0:
-            
+
             stats = summary_stats(np.array(rewards[-batch_size:]))
-            logger.log_epoch(reward = stats)
-            
+            logger.log_epoch(reward=stats)
+
             # policy optimization
             optimizer.apply_gradients(zip(grad_buffer, policy.trainable_variables))
             # re-initialize buffer to zero (on-policy)
@@ -115,7 +112,7 @@ def vpg(environment='CartPole-v0', hidden_units=32, gamma=0.9,
                 grad_buffer[ix] = grad * 0
 
             print("Epoch {0} reward: {1}".format(
-                ep_number / batch_size, 
+                ep_number / batch_size,
                 np.mean(rewards[-batch_size:])))
 
         # if ctrl+c is used to stop training, the models will be saved
@@ -132,24 +129,25 @@ def vpg(environment='CartPole-v0', hidden_units=32, gamma=0.9,
 
 if __name__ == '__main__':
     import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--env', type=str, default='CartPole-v0')
     parser.add_argument('--hid', type=int, default=32)
     # use for # of hidden layers
     parser.add_argument('--l', type=int, default=1)
     parser.add_argument('--lr', type=float, default=.01)
-    parser.add_argument('--gamma', type=float, default=0.99)
+    parser.add_argument('--gamma', type=float, default=0.8)
     parser.add_argument('--seed', '-s', type=int, default=10)
     parser.add_argument('--batch', type=int, default=5)
-    parser.add_argument('--eps', type=int, default=50)
-    parser.add_argument('--lam', type=float, default=.95)
-    parser.add_argument('--save', type=str, default = '')
+    parser.add_argument('--eps', type=int, default=2000)
+    parser.add_argument('--lam', type=float, default=.97)
+    parser.add_argument('--save', type=str, default='')
     # use for logging
     # to-do: implement good logging procedure; can take inspiration from openai (medium)
     parser.add_argument('--exp_name', type=str, default='log1')
     args = parser.parse_args()
 
-    vpg(environment=args.env, hidden_units=args.hid, gamma=args.gamma, 
+    vpg(environment=args.env, hidden_units=args.hid, gamma=args.gamma,
         seed_num=args.seed, learning_rate=args.lr, num_episodes=args.eps,
         batch_size=args.batch, num_layers=args.l, fpath=args.exp_name,
-        arg_dict = vars(args), lam=args.lam, save=args.save)
+        arg_dict=vars(args), lam=args.lam, save=args.save)
